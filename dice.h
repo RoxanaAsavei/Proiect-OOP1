b069f4170b4a2180dac3c86929359823e346054b
RoxanaAsavei <RoxanaAsavei@users.noreply.github.com>
2024-03-21 01:52:02 +0200

    Rolling function

--- dice.h
+++ dice.h
@@ -5,6 +5,7 @@
 
 class Dice {
 private:
+    sf::RenderWindow& window;
     sf::RectangleShape face1;
     sf::RectangleShape face2;
     sf::RectangleShape face3;
@@ -13,35 +14,60 @@ private:
     sf::RectangleShape face6;
     const int size;
 public:
-    Dice(const int size_) : size(size_) {
+    Dice(sf::RenderWindow& window_, const int size_) : window(window_), size(size_) {
         // loading the textures for the six sides of the dice
         sf::Texture texture;
 
-        texture.loadFromFile("C:\\Users\\roxan\\OneDrive\\Desktop\\proiect poo\\Proiect-OOP1\\dice sides\\dice-six-faces-six.png");
+        texture.loadFromFile("Proiect-OOP1/dice sides/dice-six-faces-six.png");
         face6.setSize(sf::Vector2f(size, size));
         face6.setTexture(&texture);
 
-        texture.loadFromFile("C:\\Users\\roxan\\OneDrive\\Desktop\\proiect poo\\Proiect-OOP1\\dice sides\\dice-six-faces-five.png");
+        texture.loadFromFile("Proiect-OOP1/dice sides/dice-six-faces-five.png");
         face5.setSize(sf::Vector2f(size, size));
         face5.setTexture(&texture);
 
-        texture.loadFromFile("C:\\Users\\roxan\\OneDrive\\Desktop\\proiect poo\\Proiect-OOP1\\dice sides\\dice-six-faces-four.png");
+        texture.loadFromFile("Proiect-OOP1/dice sides/dice-six-faces-four.png");
         face4.setSize(sf::Vector2f(size, size));
         face4.setTexture(&texture);
 
-        texture.loadFromFile("C:\\Users\\roxan\\OneDrive\\Desktop\\proiect poo\\Proiect-OOP1\\dice sides\\dice-six-faces-three.png");
+        texture.loadFromFile("Proiect-OOP1/dice sides/dice-six-faces-three.png");
         face3.setSize(sf::Vector2f(size, size));
         face3.setTexture(&texture);
 
-        texture.loadFromFile("C:\\Users\\roxan\\OneDrive\\Desktop\\proiect poo\\Proiect-OOP1\\dice sides\\dice-six-faces-two.png");
+        texture.loadFromFile("Proiect-OOP1/dice sides/dice-six-faces-two.png");
         face2.setSize(sf::Vector2f(size, size));
         face2.setTexture(&texture);
 
-        texture.loadFromFile("C:\\Users\\roxan\\OneDrive\\Desktop\\proiect poo\\Proiect-OOP1\\dice sides\\dice-six-faces-one.png");
+        texture.loadFromFile("Proiect-OOP1/dice sides/dice-six-faces-one.png");
         face1.setSize(sf::Vector2f(size, size));
         face1.setTexture(&texture);
 
     }
+public:
+    int Roll() { // tb facuta uniform distribuita si randomizat seed
+        return rand() % 6 + 1;
+    }
 
+    void Display() {
+        int roll_value = Roll();
+        if(roll_value == 1) {
+            window.draw(face1);
+        }
+        else if(roll_value == 2) {
+            window.draw(face2);
+        }
+        else if(roll_value == 3) {
+            window.draw(face3);
+        }
+        else if(roll_value == 4) {
+            window.draw(face4);
+        }
+        else if(roll_value == 5) {
+            window.draw(face5);
+        }
+        else {
+            window.draw(face6);
+        }
+    }
 
 };
